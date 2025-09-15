ed2bfa1320a1d0c4f89a337cfa4bf792a33d8b23
Aditya Singh <adityasingh22005@gmail.com>
2025-09-16 00:37:51 +0530

    Delete marine_drive directory

--- marine_drive/api/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from detection.views import DetectionResultViewSet
-from monitoring.views import AlertViewSet
-from users.views import UserViewSet
-
-router = DefaultRouter()
-router.register(r'detections', DetectionResultViewSet, basename='detection')
-router.register(r'alerts', AlertViewSet, basename='alert')
-router.register(r'users', UserViewSet, basename='users')
